22b2015b33db91f2c495b03b068613e08729b85f
script696 <script696@yandex.ru>
2023-03-07 18:42:50 +0200

    feat: add basename to browser router

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={setupStore()}>
-      <BrowserRouter>
+      <BrowserRouter basename="/shop">
         <App />
       </BrowserRouter>
     </Provider>
